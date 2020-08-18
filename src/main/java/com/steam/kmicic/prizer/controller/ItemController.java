960a9965f3e9be124419a41caea3790fc346bc20
morgenz <pawel.stanislaw.kmita@gmail.com>
2020-08-18 22:30:28 +0200

    #3 add template, refactor controller

--- src/main/java/com/steam/kmicic/prizer/controller/ItemController.java
+++ src/main/java/com/steam/kmicic/prizer/controller/ItemController.java
@@ -4,21 +4,24 @@ import com.steam.kmicic.prizer.model.Item;
 import com.steam.kmicic.prizer.model.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ItemController {
 
     @Autowired
     ItemRepository itemRepository;
 
     @GetMapping("/item")
-    public List<Item> allItems(){
-        return (List<Item>) itemRepository.findAll();
+    public String allItems(Model model){
+        List<Item> items = (List<Item>) itemRepository.findAll();
+        model.addAttribute("items",items);
+        return "item";
     }
 
 
