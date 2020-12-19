7d8e6b6bb62ecc27a62770403172054f970cc904
morgenz <pawel.stanislaw.kmita@gmail.com>
2020-12-19 22:16:13 +0100

    #10 modify inventory parsing, code refactor

--- src/main/java/com/steam/kmicic/prizer/controller/UserController.java
+++ src/main/java/com/steam/kmicic/prizer/controller/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("/user")
@@ -18,8 +20,7 @@ public class UserController {
     UserRepository userRepository;
 
     @PostMapping("/register")
-    public void register(@RequestBody User user) {
-
+    public void register(@Valid @RequestBody User user) {
         userRepository.save(user);
     }
 
--- src/main/java/com/steam/kmicic/prizer/serivce/JsonUtils.java
+++ src/main/java/com/steam/kmicic/prizer/serivce/JsonUtils.java
@@ -1,39 +1,44 @@
 package com.steam.kmicic.prizer.serivce;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.steam.kmicic.prizer.domain.Item;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Service
 public class JsonUtils {
 
     ObjectMapper mapper = new ObjectMapper();
+    private static final String DESCRIPTION_NODE_NAME = "descriptions";
+    private static final String ASSETS_NODE_NAME = "assets";
+    private static final String CLASS_ID_NODE_FIELD = "classid";
 
-    public List<Item> mapSteamInventory(String inventory) throws JsonProcessingException {
+    public List<Item> parseSteamInventory(String inventory) throws JsonProcessingException {
+        Map<String, Item> itemMap = new HashMap<>();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         JsonNode rootNode = mapper.readTree(inventory);
-        JsonNode firstResult = rootNode.get("assets");
-        Map<Integer, Item> itemMap = new HashMap<>();
+        JsonNode firstResult = rootNode.get(DESCRIPTION_NODE_NAME);
         for (JsonNode node : firstResult) {
-            System.out.println(node);
-            Integer itemId = node.get("classid").asInt();
-            if (itemMap.containsKey(itemId)) {
-                Item item = itemMap.get(itemId);
-                item.setAmount(item.getAmount() + 1);
-                itemMap.put(itemId, item);
-            } else {
-                itemMap.put(itemId, mapper.readValue(node.toString(), Item.class));
-            }
+            itemMap.put(node.get(CLASS_ID_NODE_FIELD).asText(), mapper.readValue(node.toString(), Item.class));
         }
+        countInventoryItems(rootNode.get(ASSETS_NODE_NAME), itemMap);
         return new ArrayList<>(itemMap.values());
     }
 
+    private void countInventoryItems(JsonNode assets, Map<String, Item> items) {
+        for (JsonNode node : assets) {
+            String itemId = node.get(CLASS_ID_NODE_FIELD).asText();
+            Item item = items.get(itemId);
+            item.setAmount(item.getAmount() + 1);
+            items.put(itemId, item);
+        }
+    }
 }
